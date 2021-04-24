794f6ff62a5f7dcf011ce81093e0947571a4e86d
Jemerson Maia <jfnandopr@gmail.com>
2021-04-24 10:19:00 -0300

    jquery

---
+++ atividade/js/app.js
@@ -0,0 +1,42 @@
+
+$(document).ready(function (){
+    
+    var formNoticias = $("#form-noticias");
+    formNoticias.on("submit", function(){
+        try {
+            var json = recordFromForm(formNoticias);
+            addDataTable(json);
+        } catch (e){
+            console.error(e);
+        }
+        return false;
+    });
+
+    function recordFromForm(form){
+        var inputs = form.find('input[type="text"], textarea');
+        var json = "";
+        inputs.each(function(idx, input){
+            var name = $(input).attr("name");
+            var value = $(input).val();
+            if (json !== "")
+                json += ",";
+            
+            json += `"${name}": "${ value.trim() }"`;
+            console.log(json);
+        });
+        json = `{${json}}`;
+        return JSON.parse(json);
+    }
+
+    function addDataTable(noticiajson){
+        var tbody = $("#table-noticias tbody");
+        var tr = $("<tr></tr>");
+        var tdTitulo = $("<td></td>");
+        var tdIntroducao = $("<td></td>");
+        tdTitulo.text(noticiajson['titulo']);
+        tdIntroducao.text(noticiajson['introducao']);
+        tr.append(tdTitulo, tdIntroducao);
+        tbody.append(tr);
+    }
+
+});
