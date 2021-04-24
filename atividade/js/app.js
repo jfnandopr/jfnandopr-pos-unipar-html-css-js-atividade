eddbb33b03b6ab25cc8c68d8056376c005c27f50
Jemerson Maia <jfnandopr@gmail.com>
2021-04-24 17:08:24 -0300

    webstorage

--- atividade/js/app.js
+++ atividade/js/app.js
@@ -20,16 +20,15 @@ $(document).ready(() => {
         const pares = (value) => value % 2 ==0;
 
         console.log("direto " + array.filter(pares).map(mult));
-
-
     }
 
     exemplosArray();
-    
+
     var formNoticias = $("#form-noticias");
     formNoticias.on("submit", () => {
         try {
             var json = recordFromForm(formNoticias);
+            saveRecord(json);
             addDataTable(json);
         } catch (e){
             console.error(e);
@@ -61,7 +60,7 @@ $(document).ready(() => {
         tdTitulo.text(noticiajson['titulo']);
         tdIntroducao.text(noticiajson['introducao']);
 
-        var remover = $('<a href="#"></a>');
+        var remover = $('<a></a>');
         remover.html('<i class="bi-trash"></i> Remover');
         remover.addClass("btn btn-sm btn-danger");
         tdAcoes.append(remover);
@@ -75,13 +74,50 @@ $(document).ready(() => {
     }
 
     const removeRow = (tr) => {
+        let idx = tr.index();
+        
         tr.remove();
         showRowCount();
+
+        let data = loadData();
+        if (data.length > 0) {
+            data.splice(idx, 1);
+            saveData(data);
+        }
     }
 
-    const showRowCount = () => {
-        var trs = $("#table-noticias tbody tr");
-        var total = trs.length;
-        $("#table-noticias tfoot tr td span").text(total);
+    const showRowCount = () => $("#table-noticias tfoot tr td span").text(countRow());
+    
+    const countRow = () => $("#table-noticias tbody tr").length;
+
+    const STORAGE_NAME = "news";
+
+    const saveRecord = (record) => {
+        let data = loadData();
+        data.push(record);
+        saveData(data);
     }
+
+    const saveData = (data) => {
+        data = JSON.stringify(data);
+        localStorage.setItem(STORAGE_NAME, data);
+    }
+
+    const loadData = () => {
+        let data = localStorage.getItem(STORAGE_NAME);
+        if (!data)
+            data = []
+        else
+            data = JSON.parse(data);
+        return data;
+    }
+
+    const loadTable = () => {
+        let data = loadData();
+        for (json of data){
+            addDataTable(json);
+        }
+    }
+    
+    loadTable();
 });
